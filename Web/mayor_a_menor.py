71324337340b37217d30b0ca81da158858edfc75
Andy Cajas <cajasandres33@gmail.com>
2024-11-24 15:38:08 -0500

    Add files via upload

---
+++ Web/mayor_a_menor.py
@@ -0,0 +1,10 @@
+# Solicitar un número al usuario
+numero = float(input("Por favor, ingrese un número: "))
+
+# Determinar si el número es mayor o menor que 10
+if numero > 10:
+    print("El número es mayor que 10.")
+elif numero < 10:
+    print("El número es menor que 10.")
+else:
+    print("El número es igual a 10.")
